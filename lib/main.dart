b45e5ab5cd0ef17107bdbbdaeb25f8dc05130f24
azcoder97 <azcoder97@gmail.com>
2025-09-12 23:41:18 +0530

    Intialize the Firebase in chatterBox

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:chatter_box/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'app.dart';
@@ -5,5 +7,6 @@ import 'app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init(); // 🔥 initialize before running app
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
