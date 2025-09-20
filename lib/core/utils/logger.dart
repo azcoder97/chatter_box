3fa4c69e651996e0c24d3f2419b69ec3190169a1
azcoder97 <azcoder97@gmail.com>
2025-09-20 13:01:11 +0530

    solved the print time error

--- lib/core/utils/logger.dart
+++ lib/core/utils/logger.dart
@@ -1,13 +1,12 @@
-// lib/core/services/logger.dart
 import 'package:logger/logger.dart';
 
 final Logger log = Logger(
   printer: PrettyPrinter(
-    methodCount: 2, // methods to show
-    errorMethodCount: 5,
+    methodCount: 2, // number of method calls to be displayed
+    errorMethodCount: 5, // number of method calls when error is thrown
     lineLength: 50,
     colors: true,
     printEmojis: true,
-    printTime: true,
+    dateTimeFormat: DateTimeFormat.onlyTimeAndSinceStart, // ✅ use this instead of printTime
   ),
 );
